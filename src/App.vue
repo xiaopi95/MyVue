eb899cec2d0a8909f9fbb6d363080db08ad16193
xiaopi95 <1176827428@qq.com>
2020-08-15 13:57:51 +0800

    0.1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <header-main></header-main>
+    <div class="main">
+      <keep-alive>
+        <transition enter-active-class="magictime twisterInUp" leave-active-class="magictime magic" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </keep-alive>
+    </div>
+    <control-player></control-player>
+    <detail-player></detail-player>
+  </div>
+</template>
+
+<script>
+import HeaderMain from './components/header/header-main'
+import ControlPlayer from './components/control-player'
+import DetailPlayer from './components/detail-player'
+export default {
+  components: {
+    HeaderMain,
+    ControlPlayer,
+    DetailPlayer
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#app {
+  .main {
+    margin-top: 94px;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import mint from 'mint-ui'
+import axios from 'axios'
+import 'mint-ui/lib/style.css'
+import './assets/styles/base.css'
+import './assets/styles/reset.less'
+// eslint-disable-next-line
+import "../node_modules/magic.css/dist/magic.css"
+
+Vue.use(mint)
+Vue.prototype.$http = axios
+
+/* eslint-disable no-new */
+window.$app = new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App />'
+})
